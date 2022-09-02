4156510c314f709d66fb34d1a24ceb8fb3c51158
Hector Anibal Castro Kenny <exhcasa@bci.cl>
2022-09-02 16:17:25 -0300

    correcciones

--- run.sh
+++ run.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 echo "### Aplicando permisos de ejecucion ###"
-chmod +x catalogo/gradlew
-chmod +x configserver/gradlew
-chmod +x servicediscovery/gradlew
-echo "### Generando Servicio catalogo ###"
-catalogo/gradlew -p catalogo clean
-catalogo/gradlew -p catalogo build
-echo "### Generando Servicio configserver ###"
-configserver/gradlew -p configserver clean
-configserver/gradlew -p configserver build
-echo "### Generando Servicio servicediscovery ###"
-servicediscovery/gradlew -p servicediscovery clean
-servicediscovery/gradlew -p servicediscovery build
+chmod +x user-service/gradlew
+chmod +x gateway-service/gradlew
+chmod +x eureka-service/gradlew
+chmod +x config-service/gradlew
+echo "### Generando Servicio user-service ###"
+user-service/gradlew -p user-service clean
+user-service/gradlew -p user-service build
+echo "### Generando Servicio gateway-service ###"
+gateway-service/gradlew -p gateway-service clean
+gateway-service/gradlew -p gateway-service build
+echo "### Generando Servicio eureka-service ###"
+eureka-service/gradlew -p eureka-service clean
+eureka-service/gradlew -p eureka-service build
+echo "### Generando Servicio config-service ###"
+config-service/gradlew -p config-service clean
+config-service/gradlew -p config-service build
 echo "### Ejecutando docker-compose ###"
-docker-compose -f docker-compose.yml -p catalogo_cristian_pardo up -d
+docker-compose -f docker-compose.yml -p catalogo_hector-castro up -d
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "### Deteniendo y limpiando ###"
-docker-compose -f docker-compose.yml -p catalogo_cristian_pardo down --remove-orphans
+docker-compose -f docker-compose.yml -p catalogo_hector-castro down --remove-orphans
