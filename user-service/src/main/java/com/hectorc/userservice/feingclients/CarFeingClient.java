e4e5c0ad07803435a070902e9db6912ffd3ffdac
Hector Anibal Castro Kenny <exhcasa@bci.cl>
2022-09-07 11:38:52 -0300

    mejoras

--- user-service/src/main/java/com/hectorc/userservice/feingclients/CarFeingClient.java
+++ user-service/src/main/java/com/hectorc/userservice/feingclients/CarFeingClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "car-service", path = "/car")
+@FeignClient(name = "carservice", path = "/car")
 public interface CarFeingClient {
 
     @PostMapping()
